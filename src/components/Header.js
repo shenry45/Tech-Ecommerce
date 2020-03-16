ec31451b49e1e37acee79731b25d6d96b170f63a
shenry45 <shawn45henry@gmail.com>
2020-03-15 20:25:42 -0700

    hero, menu, and tags section updates with css

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,25 +8,10 @@ const Header = () => {
         setAnchorEl(e.currentTarget);
     }
 
-    function handleClose() {
-        setAnchorEl(null);
-    }
-
     return <header>
         <div><p>Logo</p></div>
         <div>
-            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={openMenu}>Menu</Button>
-            <Menu
-                id='simple-menu'
-                anchorEl={anchorEl}
-                keepMounted
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-            >
-                <MenuItem onClick={handleClose}>First</MenuItem>
-                <MenuItem onClick={handleClose}>Second</MenuItem>
-                <MenuItem onClick={handleClose}>Third</MenuItem>
-            </Menu>
+            <Button onClick={openMenu}>Menu</Button>
         </div>
     </header>
 }
--- src/components/index/Tags.js
+++ src/components/index/Tags.js
@@ -2,9 +2,24 @@ import React from 'react';
 
 const Tags = () => {
     return <div class="tags">
-        <h4>Unique Identifier</h4>
-        <h4>Unique Identifier</h4>
-        <h4>Unique Identifier</h4>
+        <div>
+            <h3>Unique #1</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet in libero at efficitur. </p>
+            <hr />
+            <button>Click Me!</button>
+        </div>
+        <div>
+            <h3>Unique #2</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet in libero at efficitur. </p>
+            <hr />
+            <button>Click Me!</button>
+        </div>
+        <div>
+            <h3>Unique #3</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquet in libero at efficitur. </p>
+            <hr />
+            <button>Click Me!</button>
+        </div>
     </div>
 }
 
